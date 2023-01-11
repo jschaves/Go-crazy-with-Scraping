8f9168a1444b295bdc177497a556a5dd461ae813
Juan <juan.cha63@gmail.com>
2023-01-11 20:53:13 +0100

    Update scraping_query_trends_twitter.js

--- scraping_query_trends_twitter.js
+++ scraping_query_trends_twitter.js
@@ -9,28 +9,28 @@ const path = require('path');
 		const context = await Browser.createIncognitoBrowserContext();
 		const page=await Browser.newPage();
 		await page.setDefaultNavigationTimeout(0);
-		let search = 'Texto buscar'.replaceAll(' ', '%20');
+		let search = 'resistire.space'.replaceAll(' ', '%20');
 		await page.goto('https://twitter.com/search?q=' + search + '&src=trend_click&vertical=trends', {waitUntil: 'domcontentloaded'});
 		await page.waitForTimeout(5000);	
 		await page.screenshot({ path: "example.png" });
 		var result_ = await page.evaluate(() => {
-			var rows = document.querySelectorAll('article');
-			return Array.from(rows, row => {
+		var rows = document.querySelectorAll('article');
+		return Array.from(rows, row => {
 			let img_ = row.querySelectorAll('img');
-    			let src_ = Array.from(img_, column => column.src);
+			let src_ = Array.from(img_, column => column.src);
 			let text_ = row.querySelectorAll('div');
-    			let text1 = Array.from(text_, column => column.innerText);
+			let text1 = Array.from(text_, column => column.innerText);
 			let text__ = row.querySelectorAll('span');
-    			let text2 = Array.from(text__, column => column.innerText);
+			let text2 = Array.from(text__, column => column.innerText);
 			let link_ = row.querySelectorAll('a');
     			let link__ = Array.from(link_, column => column.href);
-				let result = src_.concat(text1).concat(text2).concat(link__).filter(Boolean);
+			let result = src_.concat(text1).concat(text2).concat(link__).filter(Boolean);
 				Array.prototype.unicos = function () {
 					return this.filter((valor, indice) => {
 						return this.indexOf(valor) === indice;
 					});
 				}
-				return result.unicos()
+				return result.unicos();
 			});
 		});
 		console.log(result_);
